11350812ab925713198838c61282f539295e8822
Declan Wood <declanwood@Declans-MacBook-Air.local>
2025-06-08 23:00:25 +1000

    Update site with latest changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/nyt-leaderboard/',  // for GitHub Pages
   plugins: [react()],
   server: {
-    open: '/submit', // or whatever route you want
-    // Use Opera's path (this works only with a CLI browser name Vite recognizes)
-    // macOS doesn't recognize "Opera" by default, so it may not work without a workaround
+    open: '/nyt-leaderboard/', // opens the root inside the base
   },
 });
